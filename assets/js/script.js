95b3a7b5e2c88452ae71ae7c1a7907199d2450a3
josceno <jdamasceno414@gmail.com>
2023-02-07 16:25:26 -0300

    first page translation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,16 @@ $(function(){
         $(".nav-item").children().eq(1).text("Experiences");
         $(".nav-item").children().eq(2).text("Skills");
         $(".nav-item").children().eq(3).text("Projects");
-        $(".txtfield").children().eq(0).children().children().text("oi3").addClass("h3");
+        
+        $("h3").eq(0).text("External Links");
+        
+        $("h3").eq(1).text("Contact info")
+        $(".txtfield").children().eq(0).children().children().eq(0).text("Phone: 79981142662");
+
+        $("h3").eq(2).text("Graduation")
+        $(".txtfield").children().eq(1).children().children().eq(0).text("College degree on going (Computer's ciencie)");
+        $(".txtfield").children().eq(1).children().children().eq(1).text("ata de incio: 03/2020  Course End's prevision: 12/2024");
+        
         $(".languages-selection").text("EN-US");
 
         $(".accordion-header").children().eq(0).text("Who am i and what i seek:");
@@ -27,6 +36,17 @@ $(function(){
         $(".nav-item").children().eq(1).text("Experiencia");
         $(".nav-item").children().eq(2).text("Habilidades");
         $(".nav-item").children().eq(3).text("Projetos");
+
+        
+        $("h3").eq(0).text("links externos");
+        
+        $("h3").eq(1).text("Informações de contato")
+        $(".txtfield").children().eq(0).children().children().eq(0).text("Telefone: 79981142662");
+        $(".txtfield").children().eq(0).children().children().eq(1).text("Email: jdamasceno414@gmail.com ");
+
+        $("h3").eq(2).text("Formação")
+        $(".txtfield").children().eq(1).children().children().eq(0).text("Ensino Superior em andamento (Cienacia da Computação)");
+        $(".txtfield").children().eq(1).children().children().eq(1).text("data de incio: 03/2020  previsão de termino: 12/2024");
 
         $(".accordion-header").children().eq(0).text("Quem eu sou e o que Procuro:");
         $(".accordion-body").children().eq(0).text(
